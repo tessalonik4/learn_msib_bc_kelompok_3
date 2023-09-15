a84e3e7be426358f19f690497de96941d931879d
tessalonik4 <damaiiyel9072@gmail.com>
2023-09-15 21:52:44 +0700

    Update Data Ke 2

---
+++ index.js
@@ -0,0 +1,53 @@
+ // Mendefinisikan variabel
+var nilai = 75;
+var nama = "John";
+var isLulus = false;
+
+// Mendefinisikan konstanta
+const PI = 3.14159265359;
+const MAX_USIA = 18;
+
+// Kondisional if-else
+if (nilai >= 70) {
+  isLulus = true;
+  console.log(nama + " lulus ujian dengan nilai " + nilai);
+} else {
+  isLulus = false;
+  console.log(nama + " tidak lulus ujian dengan nilai " + nilai);
+}
+
+// Menggunakan switch case untuk menentukan kategori nilai
+var kategori;
+
+switch (true) {
+  case nilai >= 90:
+    kategori = "A";
+    break;
+  case nilai >= 80:
+    kategori = "B";
+    break;
+  case nilai >= 70:
+    kategori = "C";
+    break;
+  case nilai >= 60:
+    kategori = "D";
+    break;
+  default:
+    kategori = "E";
+}
+
+console.log(nama + " mendapatkan kategori nilai " + kategori);
+
+// Menggunakan konstanta
+if (MAX_USIA <= 18) {
+  console.log("Anda masih di bawah usia maksimum yang diizinkan.");
+} else {
+  console.log("Anda sudah melewati usia maksimum yang diizinkan.");
+}
+
+// Menampilkan apakah lulus atau tidak
+if (isLulus) {
+  console.log(nama + " lulus ujian.");
+} else {
+  console.log(nama + " tidak lulus ujian.");
+}
